0b1b04534e83ae0ebad27c66f4c364c65616813f
ThePerkinrex <theperkinrex@gmail.com>
2022-01-31 15:59:18 +0100

    Start linker work

--- linker/src/lib.rs
+++ linker/src/lib.rs
@@ -1,3 +1,23 @@
+use armes_elf::Elf;
+use asm_ir::Program;
+
+fn link_to_elf_with_info(files: &[Elf]) -> (Elf, Vec<(String, String)>) {
+    todo!()
+}
+
+pub fn link_to_elf(files: &[Elf]) -> Elf {
+    link_to_elf_with_info(files).0
+}
+
+fn elf_to_program(elf: Elf, symbols: Vec<(String, String)>) -> Program {
+    todo!()
+}
+
+pub fn link_to_program(files: &[Elf]) -> Program {
+    let (elf, symbols) = link_to_elf_with_info(files);
+    elf_to_program(elf, symbols)
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
