ac0568569d68e159489a0fe867915ba8d4468d27
ThePerkinrex <theperkinrex@gmail.com>
2022-03-06 12:27:55 +0100

    Start adding dwarf support to assembler

--- assembler/src/config.rs
+++ assembler/src/config.rs
@@ -2,9 +2,20 @@ use std::path::PathBuf;
 
 use clap::Parser;
 
+/// An assembler for the ArmesCPU instruction set
+///
+#[cfg_attr(
+    feature = "dwarf",
+    doc = "(DWARF information generation feature enabled)  "
+)]
 #[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
+#[clap(author, version, about)]
 pub struct Config {
+    #[cfg(feature = "dwarf")]
+    #[clap(short, long = "dwarf", required = false)]
+    /// File to write debug information to
+    pub dwarf_file: Option<PathBuf>,
+
     /// Files to compile
     // #[clap(short, long)]
     pub input: Vec<PathBuf>,
